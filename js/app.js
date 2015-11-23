da66566048b091fa7e7920ff58f786917d68285a
Taylor Radtke <taylorradtke@gmail.com>
2015-11-23 14:31:30 -0500

    second commit

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ $(function() {
         accessToken: '8715856.467ede5.4d03cba121bc4b3681c5313a2867165c',
         links: true,
         limit: 8,
-        sortBy: 'most-recent',
+        sortBy: 'most-liked',
         resolution: 'standard_resolution',
         template: '<div class="col-xs-12 col-sm-6 col-md-4 col-lg-3"><div class="photo-box"><div class="image-wrap"><a href="{{link}}"><img src="{{image}}"></a><div class="likes">{{likes}} Likes</div></div><div class="description">{{caption}}<div class="date">{{model.date}}</div></div></div></div>'
     });
